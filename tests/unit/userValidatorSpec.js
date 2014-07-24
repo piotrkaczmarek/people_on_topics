845a1f2b3c00ea13688636b212a643546e080d46
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2014-07-24 14:14:56 +0200

    Adds custom UserValidator

---
+++ tests/unit/userValidatorSpec.js
@@ -0,0 +1,48 @@
+describe('UserValidator', function() {
+  var UserValidator = require('../../app/server/lib/user_validator');
+
+  describe('.validate', function() {
+    describe('not valid', function() {
+      describe('when name is blank', function() {
+        var user = {
+          name: '',
+          sex: 'male',
+          age: 15
+        }
+
+        it("it should return that it's not valid", function() {
+          expect(UserValidator.validate(user).valid).toBe(false);
+        });
+        it('it should return error message', function() {
+          expect(UserValidator.validate(user).errors.name).toBeDefined();
+        });
+      });
+      describe('when there is no name attribute', function() {
+        var user = {
+          sex: 'male',
+          age: 15
+        }
+        it("it should return that it's not valid", function() {
+          expect(UserValidator.validate(user).valid).toBe(false);
+        });
+        it('it should return error message', function() {
+          expect(UserValidator.validate(user).errors.name).toBeDefined();
+        });
+      });
+    });
+    describe('when valid', function() {
+      var user = {
+        name: 'Bob',
+        sex: 'male',
+        age: 15
+      }
+      it('should return that it is valid', function(){
+        expect(UserValidator.validate(user)).toEqual({valid: true})
+      })
+
+    })
+
+  });
+
+
+});
