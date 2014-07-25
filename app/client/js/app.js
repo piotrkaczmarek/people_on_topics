30c3a98e4f50c420fc9700a2a9046776bc252fb5
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2014-07-25 12:54:41 +0200

    Adds userCtrl on angular and adds start method to it

--- app/client/js/app.js
+++ app/client/js/app.js
@@ -2,17 +2,29 @@
 
 var app = angular.module('peopleOnTopicsApp', []);
 
+app.controller('userCtrl', function($http) {
+  var user = this;
+  user.name = '';
+  user.sex = '';
+  user.age = '';
+
+  this.start = function() {
+    $http.post('/sign_in', user).success(function(data) {
+      user.name = data.user_name;
+      console.log('Got: '+ data);
+    });
+
+
+  }
+
+});
+
 app.directive('userProfile', function(){
   return {
     restrict: 'E',
     templateUrl: 'views/user_profile.html',
-    controller: function() {
-      var user = this;
-      user.name = '';
-      user.sex = '';
-      user.age = '';
-    },
-    controllerAs: 'userCtrl'
+    controller: 'userCtrl',
+    controllerAs: 'user'
   };
 
 });
