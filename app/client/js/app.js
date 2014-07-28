2a6b5f4e7341d9abcdf4749787eab4b9aafe9c80
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2014-07-28 16:34:52 +0200

    Adds userList directive and socket factory

--- app/client/js/app.js
+++ app/client/js/app.js
@@ -2,20 +2,47 @@
 
 var app = angular.module('peopleOnTopicsApp', []);
 
-app.controller('userCtrl', function($http) {
+app.factory('socket', function() {
+  var _socket;
+  var _connect = function(token) {
+    _socket = io.connect('http://0.0.0.0:8080', {
+      'query': 'token='+token
+    }).on('connect', function() {
+      console.log('Socket authenticated');
+      return;
+    }).on('joins', function(user) {
+      console.log(user,' joined!');
+    }).on('leaves', function(user) {
+      console.log(user,' left!');
+    });
+  };
+  return {
+    connect: _connect
+  }
+});
+
+app.factory('users', function($http) {
+  var _getUsers = function(callback) {
+    $http.get('/users').success(function(data) {
+      callback(data);
+    });
+  };
+  return {
+    getUsers: _getUsers
+  }
+});
+
+app.controller('userProfileCtrl', function($http, $scope, users, socket) {
   var user = this;
   user.name = '';
   user.sex = '';
   user.age = '';
-
   this.start = function() {
     $http.post('/sign_in', user).success(function(data) {
       user.name = data.user_name;
-      io.connect('http://0.0.0.0:8080', {
-        'query': 'token='+data.token
-      }).on('connect', function() {
-        console.log('Socket authenticated');
-        return;
+      socket.connect(data.token);
+      users.getUsers(function(data){
+        $scope.users = data;
       });
     });
   }
@@ -25,8 +52,23 @@ app.directive('userProfile', function(){
   return {
     restrict: 'E',
     templateUrl: 'views/user_profile.html',
-    controller: 'userCtrl',
+    controller: 'userProfileCtrl',
     controllerAs: 'user'
   };
+});
 
+app.controller('userListCtrl', function($scope, $http, users) {  
+  users.getUsers(function(data){
+    $scope.users = data;
+  });
+});
+
+
+app.directive('userList', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/user_list.html',
+    controller: 'userListCtrl',
+    controllerAs: 'userList'
+  };
 });
