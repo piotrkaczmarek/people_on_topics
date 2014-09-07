2cb2230e9c6d6c4fbf1bb763f0858ca4b4caf221
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2014-09-08 01:07:57 +0200

    Restructures server/app.js

--- app/server/app.js
+++ app/server/app.js
@@ -1,44 +1,73 @@
 (function() {
   "use strict";
   var express = require('express'),
-    app = express(),
-    server = require('http').Server(app),
+    http = require('http'),
     fs = require('fs'),
     path = require('path'),
     redis = require('redis'),
     routes = require('./routes'),
     socketController = require('./socketController'),
-    io = require('socket.io').listen(server),
+    socketio = require('socket.io'),
     socketIoJwt = require('socketio-jwt'),
-    redisPublisher = redis.createClient(),
-    redisSubscriber = redis.createClient(),
     socketRedisCoordinator = require('./socketRedisCoordinator'),
     elasticsearch = require('elasticsearch'),
     UsersElasticSearch = require('./usersElasticSearch').UsersElasticSearch;
 
-  var esClient = elasticsearch.Client({
-    host: 'localhost:9200'
-  });
-  var usersDAO = new UsersElasticSearch(esClient);
-        
-  var argv = require('minimist')(process.argv.slice(2));
-  var port =  argv.p || 8080;
-  var socket_url = 'http://localhost:'+port; 
-  var token_secret = fs.readFileSync(path.resolve('app/server/config/token.secret'));
+
+  var Application = function() {
+    var self = this;
+
+    var setupVariables = function() {
+      self.ipaddress = 'localhost';
+      var argv = require('minimist')(process.argv.slice(2));
+      self.port =  argv.p || 8080;
+      self.socket_url = 'http://localhost:'+self.port; 
+      self.token_secret = fs.readFileSync(path.resolve('app/server/config/token.secret'));
+      self.elasticsearch_host = 'localhost:9200';
+    };
+    var setupDB = function() {
+      var esClient = elasticsearch.Client({
+        host: self.elasticsearch_host
+      });
+      self.usersDAO = new UsersElasticSearch(esClient);
+    };
+    var setupServer = function() {
+      self.expressApp = express();
+      self.server = http.Server(self.expressApp);
+
+      self.expressApp.use(express.bodyParser());
+      self.expressApp.configure(function() {  
+        self.expressApp.set('views',path.resolve('./app/client/views'));
+        self.expressApp.set('view engine', 'jade');
+        self.expressApp.use(express.static(path.resolve('./app/client')));
+      });
+    };
+    var setupRoutes = function() {
+      routes(self.expressApp, self.usersDAO,self.socket_url, self.token_secret);
+    };
+    var setupSocket = function() {
+      self.io = socketio.listen(self.server);
+      var redisPublisher = redis.createClient();
+      var redisSubscriber = redis.createClient();
+      socketController(redisPublisher, self.io, socketIoJwt, self.token_secret);
+      socketRedisCoordinator(redisSubscriber, self.usersDAO, self.io);
+    }
+    self.initialize = function() {
+      setupVariables();
+      setupDB();
+      setupServer();
+      setupRoutes();
+      setupSocket();
+    };
+    self.start = function() {
+      self.server.listen(self.port,self.ipaddress);
+      console.log('Express server listening on ',self.ipaddress,':',self.port);
+    };
+  };
+
+  var application = new Application();
+  application.initialize();
+  application.start();
 
 
-  app.use(express.bodyParser());
-
-  app.configure(function() {  
-    app.set('views',path.resolve('./app/client/views'));
-    app.set('view engine', 'jade');
-    app.use(express.static(path.resolve('./app/client')));
-  });
-
-  routes(app, usersDAO,socket_url, token_secret);
-  socketController(redisPublisher, io, socketIoJwt, token_secret);
-  socketRedisCoordinator(redisSubscriber, usersDAO, io);
-
-  server.listen(port);
-  console.log('Express server listening on port '+port);
 }());
